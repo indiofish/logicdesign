a940ff9df90bff60fb24b8cf021fa20112e0161d
thoum <indiofish@gmail.com>
2015-06-14 21:45:05 +0900

    fixed syntax error

--- final/watchController/Watch.v
+++ final/watchController/Watch.v
@@ -4,9 +4,11 @@
 `include "Mod13Counter.v" //hour 0 digit
 
 //able to set time 
+//TODO need to have defauls time input?
 module Watch (
   output wire[3:0] hr1, hr0, min1, min0, sec1, sec0,
   output daypass,
+  //input[3:0] in_hr1, in_hr0, in_min1, in_min0, in_sec1, in_sec0,
   input start_resume,reset, stop, clk, setTime
 );
 
@@ -19,5 +21,19 @@ Mod10Counter Min0(min0,min1clk,start_resume,reset,stop,min0clk);
 Mod6Counter Min1(min1,h0clk,start_resume,reset,stop,min1clk);
 Mod13Counter Hr0(hr0,h1clk,start_resume,reset,stop,h0clk);
 Mod2Counter Hr1(hr1,daypass,start_resume,reset,stop,h1clk);
-  
+
+//always @ (posedge clk)
+//begin
+  //hr1 = in_hr1;
+  //hr0 = in_hr0;
+  //min1 = in_min1;
+  //min0 = in_min0;
+  //sec1 = in_sec1;
+  //sec0 = in_sec0;
+//end
+
+
+always @ (setTime)
+begin
+end
 endmodule
